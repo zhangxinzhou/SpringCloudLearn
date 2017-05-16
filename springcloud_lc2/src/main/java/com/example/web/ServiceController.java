e59c4e16584b27111d148daa7a6ce284a8d6650f
aabbcc5050 <aabbcc5050@qq.com>
2017-05-16 14:31:10 +0800

    高可用euerka服务(euerka之间互相注册)

---
+++ springcloud_lc2/src/main/java/com/example/web/ServiceController.java
@@ -0,0 +1,31 @@
+package com.example.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class ServiceController {
+
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	
+    @Autowired
+    private DiscoveryClient client;
+    
+    @SuppressWarnings("deprecation")
+	@RequestMapping(value = "/add" ,method = RequestMethod.GET)
+    public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
+        ServiceInstance instance = client.getLocalServiceInstance();
+        Integer r = a + b;
+        logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
+        return r;
+    }
+}
