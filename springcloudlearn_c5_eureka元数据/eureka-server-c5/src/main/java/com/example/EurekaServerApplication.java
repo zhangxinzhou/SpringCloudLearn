71557ae9adba4dfa52f9fe15a766fc55afd50bb0
aabbcc5050 <aabbcc5050@qq.com>
2017-06-22 06:56:53 +0800

    000

--- springcloudlearn_c5_eureka元数据/eureka-server-c5/src/main/java/com/example/EurekaServerApplication.java
+++ springcloudlearn_c5_eureka元数据/eureka-server-c5/src/main/java/com/example/EurekaServerApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EurekaServerApplication {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {	
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 }
