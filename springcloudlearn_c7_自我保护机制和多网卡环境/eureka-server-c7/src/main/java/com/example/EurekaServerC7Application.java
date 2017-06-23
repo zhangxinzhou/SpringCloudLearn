924bae601cb12ea157d9f42586224c661bfdd5cd
aabbcc5050 <aabbcc5050@qq.com>
2017-06-24 07:09:55 +0800

    110

---
+++ springcloudlearn_c7_自我保护机制和多网卡环境/eureka-server-c7/src/main/java/com/example/EurekaServerC7Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerC7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerC7Application.class, args);
+	}
+}
