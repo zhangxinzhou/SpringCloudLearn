bc53463c7f6d977ac87ce7caf5d0c1c5bec94600
aabbcc5050 <aabbcc5050@qq.com>
2017-07-02 17:07:31 +0800

    feign参数绑定

---
+++ springcloudlearn_1/l09_feign参数绑定/feign-consumer/src/main/java/com/example/controller/ConsumerController.java
@@ -0,0 +1,31 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.User;
+import com.example.service.HelloService;
+
+@RestController
+public class ConsumerController {
+
+	@Autowired
+	HelloService helloService;
+	
+	@RequestMapping(value="/feign-consumer",method=RequestMethod.GET)
+	public String helloConsumer(){
+		return helloService.hello();
+	}
+	
+	@RequestMapping(value="/feign-consumer2",method=RequestMethod.GET)
+	public String helloConsumer2(){
+		StringBuilder sb=new StringBuilder();
+		sb.append(helloService.hello()).append("\n");
+		sb.append(helloService.hello("DIDI")).append("\n");
+		sb.append(helloService.hello("DIDI",30)).append("\n");
+		sb.append(helloService.hello(new User("DIDI",30))).append("\n");
+		return sb.toString();
+	}
+}
