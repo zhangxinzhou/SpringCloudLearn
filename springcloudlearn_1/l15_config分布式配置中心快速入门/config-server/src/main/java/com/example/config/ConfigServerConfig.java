c6a4623741259ac6f72a8a2e5c1bfe7b37be032b
aabbcc5050 <aabbcc5050@qq.com>
2017-07-08 14:44:25 +0800

    111

---
+++ springcloudlearn_1/l15_config分布式配置中心快速入门/config-server/src/main/java/com/example/config/ConfigServerConfig.java
@@ -0,0 +1,10 @@
+package com.example.config;
+
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@EnableConfigServer
+@Configuration
+public class ConfigServerConfig {
+
+}
