1af267d739c8120cbd4671c833584529402a324e
aabbcc5050 <aabbcc5050@qq.com>
2017-07-09 16:03:05 +0800

    111

---
+++ springcloudlearn_1/l18_config_server,spring cloud bus整合/rabbit-test/src/test/java/com/example/RabbitTestApplicationTests.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.rabbitmq.Sender;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RabbitTestApplicationTests {
+
+	@Autowired
+	private Sender sender;
+	
+	@Test
+	public void contextLoads() {
+		sender.send();
+	}
+
+}
