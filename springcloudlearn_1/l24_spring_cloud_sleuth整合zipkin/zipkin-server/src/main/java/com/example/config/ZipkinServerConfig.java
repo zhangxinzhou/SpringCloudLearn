552c27a0679711578cd11ea250a463cd34eacf50
aabbcc5050 <aabbcc5050@qq.com>
2017-07-16 10:26:04 +0800

    111

---
+++ springcloudlearn_1/l24_spring_cloud_sleuth整合zipkin/zipkin-server/src/main/java/com/example/config/ZipkinServerConfig.java
@@ -0,0 +1,11 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import zipkin.server.EnableZipkinServer;
+
+@EnableZipkinServer
+@Configuration
+public class ZipkinServerConfig {
+
+}
