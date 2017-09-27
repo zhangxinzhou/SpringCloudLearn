7b74149241b37b04a48b7b1f1751e08f8e6be3d4
aabbcc5050 <aabbcc5050@qq.com>
2017-09-28 07:37:41 +0800

    000

--- springcloudlearn_2/template/microservice-provider-user/src/main/java/com/example/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long>{
-
-}
--- springcloudlearn_2/template/microservice-provider-user/src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-drop table if EXISTS user;
-create table user(
-id BIGINT,
-username varchar(40),
-name varchar(20),
-age int(3),
-balance DECIMAL(10,2),
-primary key (id)
-);
