447e6c794c1365d878dcbe72a6e37310874efbf9
aabbcc5050 <aabbcc5050@qq.com>
2017-09-30 07:49:15 +0800

    000

---
+++ springcloudlearn_2/springcloudlearn_c43_springCloudSleuth_rabbitmq_elasticsearch/microservice-trace-zipkin-server/src/main/java/com/example/MicroserviceTraceZipkinServerApplication.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import zipkin.server.EnableZipkinServer;
+
+@EnableZipkinServer
+@SpringBootApplication
+public class MicroserviceTraceZipkinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceTraceZipkinServerApplication.class, args);
+	}
+}
