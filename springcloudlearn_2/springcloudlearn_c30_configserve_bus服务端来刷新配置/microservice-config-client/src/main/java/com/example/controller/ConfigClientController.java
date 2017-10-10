96166b09fab3a970cadb3a395b7bf0c6f257e505
aabbcc5050 <aabbcc5050@qq.com>
2017-10-11 07:53:11 +0800

    000

--- springcloudlearn_2/springcloudlearn_c30_configserve_bus服务端来刷新配置/microservice-config-client/src/main/java/com/example/controller/ConfigClientController.java
+++ springcloudlearn_2/springcloudlearn_c30_configserve_bus服务端来刷新配置/microservice-config-client/src/main/java/com/example/controller/ConfigClientController.java
@@ -11,8 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ConfigClientController {
 
-	//@Value("${profile}")
-	@Value("${from}")
+	@Value("${profile}")
 	private String profile;
 	
 	@Autowired
